65bb21e21fba41af3e5c76d12e481be081760206
Pablo-O9 <78048016+Pablo-O9@users.noreply.github.com>
2022-06-06 18:54:33 +0200

    a

--- Inicio.php
+++ Inicio.php
@@ -5,15 +5,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
     <title>Inicio</title>
 </head>
 
 <body>
-    <header>
-        <nav>
-            <?php include "_navbar.php" ?>
-        </nav>
-    </header>
+
+    <!-- Header -->
+    <section id="header">
+        <div class="header container">
+            <div class="nav-bar">
+                <div class="brand">
+                    <a href="#hero">
+                        <h1><span>G</span>ran<span>J</span>uca</h1>
+                    </a>
+                </div>
+                <div class="nav-list">
+                    <div class="hamburger">
+                        <div class="bar"></div>
+                    </div>
+                    <ul>
+                        <li><a href="#inicio" data-after="Inicio">Inicio</a></li>
+                        <li><a href="#services" data-after="Service">Services</a></li>
+                        <li><a href="#projects" data-after="Projects">Projects</a></li>
+                        <li><a href="#about" data-after="About">About</a></li>
+                        <li><a href="#contact" data-after="Contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Header -->
+
+
+    <!-- Hero Section  -->
+    <section id="inicio">
+        <div class="hero container">
+            <div>
+                <h1>Bienvenido a<span></span></h1>
+                <h1>GranJuca<span></span></h1>
+                <a href="#projects" type="button" class="cta">Portfolio</a>
+            </div>
+        </div>
+    </section>
+    <!-- End Hero Section  -->
 
 </body>
 
